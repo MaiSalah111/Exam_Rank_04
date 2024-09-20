b6b8b0ce29da830d2b03b57bb507b4ed5f91630d
MaiSalah111 <158178517+MaiSalah111@users.noreply.github.com>
2024-09-20 08:32:49 +0000

    Microshell

---
+++ microshell.c
@@ -0,0 +1,65 @@
+#include "microshell.h"
+
+int err(char *str)
+{
+	while(*str)
+		write (2, str++, 1);
+	return(1);
+}
+
+int cd(char **argv, int i)
+{
+	if (i != 2 )
+		return err("badargument\n");
+	if (chdir(argv[1]) == -1)
+		return err("cannot change directory to "), err(argv[1]), err("\n");
+	return (0);
+}
+
+void set_pipe(int has_pipe, int *fd, int end)
+{
+	if(has_pipe && (dup2(fd [end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		err ("fatal\n");
+}
+
+int exec(char **argv, int i, char **envp)
+{
+	int has_pipe, fd[2], pid, status;
+
+	has_pipe = argv[i] && (!strcmp(argv[i], "|"));
+	if (has_pipe && pipe(fd) == -1)
+		return err("fatal\n");
+	if ((pid = fork()) == -1)
+		return err("fatal\n");
+	if (!pid)
+	{
+		argv[i] = 0;
+		set_pipe(has_pipe, fd, 1);
+		execve(*argv, argv, envp);
+		return err("error: cannot execute "), err(*argv), err("\n");
+	}
+	waitpid(pid, &status, 0);
+	set_pipe(has_pipe, fd, 0);
+	return WIFEXITED(status) && WEXITSTATUS(status);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	int i = 0;
+	int status = 0;
+	if (argc > 1)
+	{
+		while (argv[i] && argv[++i])
+		{
+			argv += i;
+			i = 0;
+			while(argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+				i++;
+			if (!strcmp(*argv, "cd"))
+				status = cd(argv, i);
+			else if (i)
+				status = exec(argv, i, envp);
+		}
+	}
+	return(status);
+}
